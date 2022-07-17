ded1782c655ead35783f3bfad8bcde2c8fcaa74a
Daniel <mikyjo2012@gmail.com>
2022-07-17 11:40:04 -0500

    finish exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numOfRepetition) {
+    let result = '';
+    for(i = 0; i < numOfRepetition; i ++) {
+        result += stringToRepeat;
+    }
+    if(numOfRepetition < 0) {
+        result = 'ERROR';
+    }
+    return result;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
