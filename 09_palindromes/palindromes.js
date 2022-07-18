7db1adec027644373c4abb47b57842da09064ec0
Daniel <mikyjo2012@gmail.com>
2022-07-18 14:10:37 -0500

    finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+    x = x.replace(/[&\/\\#,+()$~%.'":*?<>{}!]/g, "");
+    x = x.replace(/\s/g, '');
+    x = x.toUpperCase();
+    let firstHalf = '';
+    for(i = 0; i < x.length; i ++) {
+        firstHalf += x[i];
+    }
+    let secondHalf = '';
+    for(i = x.length - 1; i >= 0; i --) {
+        secondHalf += x[i];
+    }
+    if(firstHalf == secondHalf) return true;
+    else return false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
