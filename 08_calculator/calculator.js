8d670266ad41de407bad98c8ef29c8993414d685
Daniel <mikyjo2012@gmail.com>
2022-07-18 13:51:42 -0500

    finish exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  let s = 0;
+	for(i = 0; i < x.length; i ++) {
+    s += x[i];
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  let startValue;
+  if(x.length > 0)
+    startValue = x[0];
+  for(i = 1; i < x.length; i ++) {
+    startValue *= x[i];
+  }
+  return startValue;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+  else {
+    return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
